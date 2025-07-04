108bba6ce01bb0a6f61a80ff9b63a4549a78c9f9
julia3106 <julia.miranda.luiz@escola.pr.gov.br>
2025-07-04 12:13:31 -0300

    lll

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,50 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    'Ocorreu no dia 15 de Novembro de 1889.'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma molécula?',
+    'É  um conjunto de dois ou mais átomos ligados.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um penâlti?',
+    'É quando um jogador é derrubado dentro da área.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o maior país em extensão territorial no mundo?',
+    'O maior país é a Rússia.'
+)
+criaCartao(
+    'Português',
+    'Na frase "Os alunos chegram cedo na escola" o sujeito é?',
+    'O sujeito é os alunos'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Ciências',
+    'Qual é a principal função dos glóbulos veremlhos no sangue humano?',
+    'Transportar oxigênio dos pulmões para as células do corpo através da hemoglobina.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Física',
+    'O que é a força da gravidade?',
+    'É a força que atrai os corpos para o centro da Terra.'
+)
+criaCartao(
+    'Sociologia',
+    'O que é cultura na sociologia?',
+    'É um conjuto de valores, crenças, hábitos, normas e conhecimentos compartilhados por um grupo social.'
+)
+criaCartao(
+    'Ciências',
+    'Qual é a principal função dos glóbulos veremlhos no sangue humano?',
+    'Transportar oxigênio dos pulmões para as células do corpo através da hemoglobina.'
 )
